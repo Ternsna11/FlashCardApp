30c7ef3d6c1b296c32d848e5ef5939fefb2972ab
Nick Terns <ternsna11@gmail.com>
2021-06-29 09:56:24 -0500

    first commit

--- src/Card/Create/index.js
+++ src/Card/Create/index.js
@@ -2,10 +2,15 @@ import React { useEffect, useState } from "react";
 import { Link, useHistory, useParams } from "react-router-dom";
 import { createCard, readDeck } from "../../utils/api";
 import CardForm from "../From";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function CreateCard(){
     const history = useHistory();
     const { deckId } = useParams(); // destructuring deckId from useParams
-    const [deck, setDeck] = useState({cards: []}) // making our useState then 
+    const [deck, setDeck] = useState({cards: []}) 
+    // making our useState then giving it default with object and cards and the value empty array 
+    useEffect(()=>{
+        readDeck(deck.Id).then(setDeck);
+    }, [deckId]) // calling useEffect to render when readDeck function is called with deck.Id as arguement then we run setDeck
+    
 }
