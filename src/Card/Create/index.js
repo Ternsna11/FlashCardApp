5bda3706597535c1a504ee6dc0d56c3cc56478df
Nick Terns <ternsna11@gmail.com>
2021-06-29 10:04:10 -0500

    commit

--- src/Card/Create/index.js
+++ src/Card/Create/index.js
@@ -1,7 +1,7 @@
 import React { useEffect, useState } from "react";
 import { Link, useHistory, useParams } from "react-router-dom";
 import { createCard, readDeck } from "../../utils/api";
-import CardForm from "../From";
+// import CardForm from "../From";
 import { useEffect, useState } from "react";
 
 function CreateCard(){
